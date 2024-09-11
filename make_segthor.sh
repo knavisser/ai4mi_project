58466dc5316b5229037607d4c551c6990bc5411b
Dijs Boeke <scur2445@int6.local.snellius.surf.nl>
2024-09-11 13:21:42 +0200

    Added SLURM job script for extraction and slicing for Segthor with multi-processing

---
+++ make_segthor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=TrainingTest
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=04:00:00
+#SBATCH --output=slurm_output_%A.out
+
+cd /home/scur2445/ai4mi_project
+
+source ai4mi/bin/activate
+
+make data/SEGTHOR
